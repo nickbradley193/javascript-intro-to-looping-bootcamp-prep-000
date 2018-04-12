764a4d8088b521151cac4a10480a9923d3311bc6
Nick Bradley <nick.bradley.193@pm.me>
2018-04-12 05:10:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   var stringToAdd;
-  for (i = 0; i < 25; i++) {
+  for (let i = 0; i < 25; i++) {
     if (i === 1) {
       stringToAdd = 'I am 1 strange loop.';
     } else {
@@ -14,6 +14,7 @@ function whileLoop(number) {
   while (number >= 0) {
     console.log(number--);
   }
+  return 'done';
 }
 
 function maybeTrue() {
